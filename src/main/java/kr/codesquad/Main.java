4a0151c050badf0646f4fb162003347af3f0fde4
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-05 12:26:16 +0900

    메인 파일 쓸모없는 코드 지움

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,8 +1,4 @@
 package kr.codesquad;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
