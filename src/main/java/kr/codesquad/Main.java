2b492b9dbc16b8614be0316706d970c5ebb0bc5d
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-05 17:43:41 +0900

    틀린 값이 너무 많이 들어왔을 때의 예외처리 메인함수에서 스택오버플로우 발생 시 메시지를 출력하고 종료되도록 구현

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -3,6 +3,11 @@ package kr.codesquad;
 public class Main {
     public static void main(String[] args) {
         App application = new App();
-        application.play();
+        try{
+            application.play();
+        } catch(StackOverflowError e) {
+            System.out.println("형식에 벗어난 입력이 비정상적으로 많이 발생하였습니다.");
+            System.out.println("프로그램을 종료합니다.");
+        }
     }
 }
